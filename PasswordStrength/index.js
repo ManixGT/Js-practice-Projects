ef22dc0a42ebf82872a4b4780aecae0be9d48d25
Suryamani Gupta <manigupta28201@gmail.com>
2024-07-12 04:15:34 +0530

    Password Strength

---
+++ PasswordStrength/index.js
@@ -0,0 +1,20 @@
+const input = document.querySelector("#input");
+const p = document.querySelector("p");
+input.addEventListener("input", (event) => {
+  const inputValue = event.target.value;
+  const regex = /[!@#$%^&*(),.?":{}|<>]/g;
+  const hasSpecialChar = regex.test(inputValue);
+  const hasUpperCase = /[A-Z]/.test(inputValue);
+  const hasNumber = /[0-9]/.test(inputValue);
+  const isLongEnough = inputValue.length > 8;
+
+  if (hasNumber && hasSpecialChar && hasUpperCase && isLongEnough) {
+    p.style.color = "green";
+    p.innerText = "Valid";
+  } else if (inputValue.length === 0) {
+    p.innerText = "";
+  } else {
+    p.style.color = "red";
+    p.innerText = "Less Strength";
+  }
+});
--- QR-code/index.js
+++ QR-code/index.js
@@ -1,6 +1,6 @@
 const input = document.querySelector("#input");
 const image = document.querySelector("#img-qr");
 
-input.addEventListener("change", (event) => {
+input.addEventListener("input", (event) => {
   image.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${event.target.value}`;
 });
